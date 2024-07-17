026bea7499109ae40bbc1ca9b159a87b0a870877
Hossein Shaemi Barzoki <hoseinshaemi@gmail.com>
2024-07-17 14:22:39 -0700

    Updated Program.cs, GitIgnore and Views

--- Program.cs
+++ Program.cs
@@ -1,7 +1,47 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using FileUploader.Models;
+using FileUploader.Repositories.Contracts;
+using FileUploader.Repositories;
+using FileUploader.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Net;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddScoped<IFileRepository, FileRespository>();
+builder.Services.AddSingleton<IAws3Services, Aws3Services>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IViewRenderService, RenderViewToString>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
+{
+    option.LoginPath = "/Account/Login";
+    // option.LogoutPath = "/Account/Logout";
+    option.ExpireTimeSpan = TimeSpan.FromDays(7);
+    option.Cookie.Name = "authentication";
+    option.AccessDeniedPath = "/Account/Login";
+    option.Events = new CookieAuthenticationEvents
+    {
+        OnRedirectToAccessDenied = context =>
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return Task.CompletedTask;
+        }
+    };
+});
+builder.Services.AddDbContext<FileUploaderContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("FileUploaderContext"));
+});
 
 var app = builder.Build();
 
@@ -13,11 +53,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
